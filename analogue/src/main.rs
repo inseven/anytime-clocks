020e5f7b6d0aa137ca52c63d93d8b00b46b807e2
Jason Morley <hello@jbmorley.co.uk>
2026-09-17 17:20:38 +0100

    fix: Add a username to the clock face (#2)

--- analogue/src/main.rs
+++ analogue/src/main.rs
@@ -61,9 +61,17 @@ fn main() {
         .build();
     rl.set_target_fps(60);
 
+    let roboto_font = rl.load_font(&thread, "resources/roboto/Roboto-Regular.ttf")
+        .expect("failed to load font");
+
     while !rl.window_should_close() && running.load(Ordering::SeqCst) {
 
-        // Get the screen / window size.
+        const NAME_SIZE: f32 = 24.0;
+        const FONT_SPACING: f32 = 0.0;
+
+        let name = "User";
+        let name_width = roboto_font.measure_text(name, NAME_SIZE, FONT_SPACING);
+
         let window_width = rl.get_screen_width();
         let window_height = rl.get_screen_height();
 
@@ -85,12 +93,15 @@ fn main() {
 
         const HOUR_HAND_LENGTH: f32 = 80.0;
         const HOUR_HAND_WIDTH: f32 = 12.0;
+        const HOUR_HAND_COLOR: Color = Color::WHITE;
 
         const MINUTE_HAND_LENGTH: f32 = 140.0;
         const MINUTE_HAND_WIDTH: f32 = 6.0;
+        const MINUTE_HAND_COLOR: Color = Color::WHITE;
 
         const SECOND_HAND_LENGTH: f32 = 160.0;
         const SECOND_HAND_WIDTH: f32 = 2.0;
+        const SECOND_HAND_COLOR: Color = Color::new(170, 170, 170, 255);
 
         d.draw_ring(
             center,
@@ -101,31 +112,33 @@ fn main() {
             100,
             Color::WHITE);
 
+        let name_position = Vector2::new(center.y - (name_width.x / 2.0), 340.0);
+        d.draw_text_codepoints(&roboto_font, name, name_position, NAME_SIZE, FONT_SPACING, SECOND_HAND_COLOR);
+
         let second = local.second() as f32 + (local.nanosecond() as f32 / 1000000000.0);
         let minute = local.minute() as f32 + (second / 60.0);
         let hour = (local.hour() % 12) as f32 + (minute / 60.0);
 
         let hour_angle = (pi_2 / 12.0) * hour - rotation;
         let hour_end = center + Vector2::new(hour_angle.cos(), hour_angle.sin()) * HOUR_HAND_LENGTH;
-        d.draw_line_ex(center, hour_end, HOUR_HAND_WIDTH, Color::WHITE);
-        d.draw_circle_v(hour_end, HOUR_HAND_WIDTH / 2.0, Color::WHITE);
-        d.draw_circle_v(center, 10.0, Color::WHITE);
+        d.draw_line_ex(center, hour_end, HOUR_HAND_WIDTH, HOUR_HAND_COLOR);
+        d.draw_circle_v(hour_end, HOUR_HAND_WIDTH / 2.0, HOUR_HAND_COLOR);
+        d.draw_circle_v(center, 10.0, HOUR_HAND_COLOR);
 
         let minute_angle = (pi_2 / 60.0) * minute - rotation;
         let minute_end = center + Vector2::new(minute_angle.cos(), minute_angle.sin()) * MINUTE_HAND_LENGTH;
-        d.draw_line_ex(center, minute_end, MINUTE_HAND_WIDTH, Color::WHITE);
-        d.draw_circle_v(minute_end, MINUTE_HAND_WIDTH / 2.0, Color::WHITE);
-        d.draw_circle_v(center, 7.0, Color::WHITE);
+        d.draw_line_ex(center, minute_end, MINUTE_HAND_WIDTH, MINUTE_HAND_COLOR);
+        d.draw_circle_v(minute_end, MINUTE_HAND_WIDTH / 2.0, MINUTE_HAND_COLOR);
+        d.draw_circle_v(center, 7.0, MINUTE_HAND_COLOR);
 
-        let second_hand_color = Color::new(170, 170, 170, 255);
         let seconds_angle = (pi_2 / 60.0) * second - rotation;
         let second_end = center + Vector2::new(seconds_angle.cos(), seconds_angle.sin()) * SECOND_HAND_LENGTH;
-        d.draw_line_ex(center, second_end, SECOND_HAND_WIDTH, second_hand_color);
-        d.draw_circle_v(second_end, SECOND_HAND_WIDTH / 2.0, second_hand_color);
+        d.draw_line_ex(center, second_end, SECOND_HAND_WIDTH, SECOND_HAND_COLOR);
+        d.draw_circle_v(second_end, SECOND_HAND_WIDTH / 2.0, SECOND_HAND_COLOR);
         let second_start = center - Vector2::new(seconds_angle.cos(), seconds_angle.sin()) * 30.0;
-        d.draw_line_ex(center, second_start, SECOND_HAND_WIDTH, second_hand_color);
-        d.draw_circle_v(second_start, SECOND_HAND_WIDTH / 2.0, second_hand_color);
-        d.draw_circle_v(center, 4.0, second_hand_color);
+        d.draw_line_ex(center, second_start, SECOND_HAND_WIDTH, SECOND_HAND_COLOR);
+        d.draw_circle_v(second_start, SECOND_HAND_WIDTH / 2.0, SECOND_HAND_COLOR);
+        d.draw_circle_v(center, 4.0, SECOND_HAND_COLOR);
 
     }
 
